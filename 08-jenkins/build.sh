8134a75fa012e1fccda1e62f81dc54c6e94bf14c
Eugene Lapeko <eugene@lapeko.info>
2020-02-08 16:52:07 +0300

    Update build.sh

--- 08-jenkins/build.sh
+++ 08-jenkins/build.sh
@@ -1,15 +1,10 @@
 export GOPATH=$WORKSPACE
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-go get github.com/tools/godep
-go get github.com/smartystreets/goconvey
-go get github.com/GeertJohan/go.rice/rice
-go get github.com/wickett/word-cloud-generator/wordyapi
-
 sed -i 's/1.DEVELOPMENT/1.$BUILD_NUMBER/g' ./rice-box.go
 
-GOOS=linux GOARCH=amd64 go build -o ./artifacts/word-cloud-generator -v .
+make
 
-md5sum artifacts/word-cloud-generator
-gzip artifacts/word-cloud-generator
-ls -l artifacts/
+md5sum artifacts/*/word-cloud-generator* >artifacts/word-cloud-generator.md5
+
+gzip artifacts/*/word-cloud-generator*
