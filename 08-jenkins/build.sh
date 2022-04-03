ddee2e2f013cad980375b6312c51aa11f9de7e4a
Alina Freydina <alina.freydina@gmail.com>
2022-04-04 00:04:49 +0300

    Updated 08-jenkins

--- 08-jenkins/build.sh
+++ 08-jenkins/build.sh
@@ -1,10 +1,16 @@
-export GOPATH=$WORKSPACE
+export GOPATH=$WORKSPACE/go
 export PATH="$PATH:$(go env GOPATH)/bin"
+
+go get github.com/tools/godep
+go get github.com/smartystreets/goconvey
+go get github.com/GeertJohan/go.rice/rice
+go get github.com/wickett/word-cloud-generator/wordyapi
+go get github.com/gorilla/mux
 
 sed -i 's/1.DEVELOPMENT/1.$BUILD_NUMBER/g' ./rice-box.go
 
-make
+GOOS=linux GOARCH=amd64 go build -o ./artifacts/word-cloud-generator -v 
 
-md5sum artifacts/*/word-cloud-generator* >artifacts/word-cloud-generator.md5
-
-gzip artifacts/*/word-cloud-generator*
+md5sum artifacts/word-cloud-generator
+gzip artifacts/word-cloud-generator
+ls -l artifacts/
